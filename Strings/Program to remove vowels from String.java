37c450b73903141b6b66805b464c4456ce0a5b69
Vinod kumar <149933876+nani986690@users.noreply.github.com>
2026-07-31 23:23:50 +0530

    Create Program to remove vowels from String.java

---
+++ Strings/Program to remove vowels from String.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    public static void main(String[] args) {
+
+        String s = "prepinsta";
+        String s1 = "";
+        s1 = s.replaceAll("[aeiou]", "");
+        System.out.println("String after removing vowel : " + s1);
+    }
+}
