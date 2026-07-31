5cde1164564fc0f01aaae5aa91691be43fb9ad0e
Vinod kumar <149933876+nani986690@users.noreply.github.com>
2026-07-31 23:12:39 +0530

    Create A character is an alphabet or not using .java

---
+++ Strings/A character is an alphabet or not using .java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		char ch='@';
+		if((ch>=97 && ch<=122)|| (ch>=65 && ch<=91)){
+		    System.out.println(ch +" "+ "is a Alphabet");
+		}
+else{
+    System.out.println(ch +" "+ "is Not Alphabet");
+}
+	}
+}
+
+//out put :@ is Not Alphabet
