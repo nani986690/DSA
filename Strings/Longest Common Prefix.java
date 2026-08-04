aa074ab48c608bd4f38ee9fa0dae319090a44358
Vinod kumar <149933876+nani986690@users.noreply.github.com>
2026-08-04 23:11:05 +0530

    Implement Longest Common Prefix algorithm

---
+++ Strings/Longest Common Prefix.java
@@ -0,0 +1,19 @@
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        String[] arr = {"flower", "flow", "flight"};
+
+        String prefix = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            while (!arr[i].startsWith(prefix)) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+
+                if (prefix.isEmpty()) {
+                    break;
+                }
+            }
+        }
+
+        System.out.println("Longest Common Prefix: " + prefix);
+    }
+}
